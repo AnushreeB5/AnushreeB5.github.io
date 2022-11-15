97c81285601705d73bea458136fbbefb7740bae0
AnushreeB5 <anushreeb@berkeley.edu>
2022-11-14 22:03:18 -0800

    attempt1atlab8

---
+++ labs/lab8/script.js
@@ -0,0 +1,20 @@
+function toggleTheme(){
+    //Identify the <body> element of the page
+    //Toggle .dark-mode css class on <body>
+   bodyElement = document.getElementById("body"); 
+   bodyElement.classList.toggle(".dark-mode"); 
+}
+
+// Use JS to get the toggle button element, and store it in a variable with a reasonable name.
+
+
+
+
+
+
+
+
+
+//Once you've done that, add the following line of code, which tells the browser: when the button is clicked on, call the toggleTheme function.
+//buttonVariable.onclick = toggleTheme;
+
